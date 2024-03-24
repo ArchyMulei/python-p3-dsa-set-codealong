08c7b3de147b787a16b60cccede5cb7e7b88d3fd
Archibald <archimulei023@gmail.com>
2024-03-24 23:08:15 +0300

    Completed assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,33 @@
+
 class MySet:
 
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+# Myset.has
+    def has(self, value):
+        return value in self.dictionary
+
+    def __str__(self):
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
+
+# Myself.add
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+# Myself.delete
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+# Myset.size
+    def size(self, value):
+        return len(self.dictionary)
+
+# bonus
+    def clear(self):
+        self.dictionary.clear()
+        return self
